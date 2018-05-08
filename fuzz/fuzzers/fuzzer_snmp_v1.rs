36283e64860e70a105c4085d8cd0af4b7c009b7a
Pierre Chifflier <chifflier@wzdftpd.net>
2018-05-08 21:58:14 +0200

    Add fuzzing files

---
+++ fuzz/fuzzers/fuzzer_snmp_v1.rs
@@ -0,0 +1,8 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate snmp_parser;
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+    let _ = snmp_parser::parse_snmp_v1(data);
+});
